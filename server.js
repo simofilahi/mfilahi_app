67fdb82b9f1803d94f6b41f8b6769fb8ba17a815
simofilahi <mohamedfilahi222@gmail.com>
2021-12-07 14:41:48 +0100

    app added

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.send("<h1>Hello from </h1>");
+});
+
+const PORT = 42;
+
+app.listen(PORT, () => {
+  console.log(`Server started running on ${PORT} ....`);
+});
