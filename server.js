9ab557e760147fa3ed0914550319f9499165c998
simofilahi <mohamedfilahi222@gmail.com>
2021-12-11 17:40:43 +0100

    updated to v2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
-  res.send("<h1>Hello from </h1>");
+  res.send("<h1>Hello from v2</h1>");
 });
 
 const PORT = 1337;
